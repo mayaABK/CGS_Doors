099e1708f95af4f9871e18f2f391db24ff5ae103
mayaActivision <mlake@activision.com>
2022-08-24 17:37:48 -0400

    rotating door

---
+++ Source/CGS_Doors/CGS_Doors.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CGS_Doors.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CGS_Doors, "CGS_Doors" );
---
+++ Source/CGS_Doors/DoorInteractionComponent.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DoorInteractionComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CGS_DOORS_API UDoorInteractionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDoorInteractionComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	FRotator DesiredRotation = FRotator::ZeroRotator;
+	FRotator StartRotation = FRotator::ZeroRotator;
+	FRotator FinalRotation = FRotator::ZeroRotator;
+
+	UPROPERTY(EditAnywhere)
+	float TimeToRotate = 1.0f;
+
+	float CurrentRotationTime = 0.0f;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
